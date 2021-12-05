cdccebd001f0e2a5b5d8c9803ef3ac91bb1af053
UNA <yunapuuu35@gmail.com>
2021-12-05 18:05:15 +0900

    TankManagerにコメントを追加

--- Assets/Scripts/Managers/TankManager.cs
+++ Assets/Scripts/Managers/TankManager.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 [Serializable]
 public class TankManager
 {
-    public Color m_PlayerColor;            
-    public Transform m_SpawnPoint;         
+    public Color m_PlayerColor; //プレイヤーが生成時に得る色            
+    public Transform m_SpawnPoint; //生成位置        
     [HideInInspector] public int m_PlayerNumber;             
-    [HideInInspector] public string m_ColoredPlayerText;
+    [HideInInspector] public string m_ColoredPlayerText; 
     [HideInInspector] public GameObject m_Instance;          
     [HideInInspector] public int m_Wins;                     
 
@@ -26,8 +26,10 @@ public class TankManager
         m_Movement.m_PlayerNumber = m_PlayerNumber;
         m_Shooting.m_PlayerNumber = m_PlayerNumber;
 
+        //テキストを特定の色に色付けする
         m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
 
+        //オブジェクトについているすべてのRendererのコンポーネントを取得し、特定の色に色付け
         MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer>();
 
         for (int i = 0; i < renderers.Length; i++)
@@ -36,12 +38,13 @@ public class TankManager
         }
     }
 
-
+    
     public void DisableControl()
     {
         m_Movement.enabled = false;
         m_Shooting.enabled = false;
 
+        //スクリプトの無効とキャンバスの無効
         m_CanvasGameObject.SetActive(false);
     }
 
@@ -51,15 +54,18 @@ public class TankManager
         m_Movement.enabled = true;
         m_Shooting.enabled = true;
 
+        //スクリプトの有効とキャンバスの有効
         m_CanvasGameObject.SetActive(true);
     }
 
 
     public void Reset()
     {
+        
         m_Instance.transform.position = m_SpawnPoint.position;
         m_Instance.transform.rotation = m_SpawnPoint.rotation;
 
+        //インスタンスを生成位置に戻す
         m_Instance.SetActive(false);
         m_Instance.SetActive(true);
     }
